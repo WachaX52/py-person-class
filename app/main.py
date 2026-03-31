10afd5b41a2f5cd3ba8f95209d096d4a44212959
Ostapenko Denys <wachax52@gmail.com>
2026-03-31 23:29:59 +0300

    Add Person class and create_person_list with spouse links

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    person_list = []
+
+    for person in people:
+        p_ = Person(person["name"], person["age"])
+        person_list.append(p_)
+
+    for person in people:
+        current_person = Person.people[person["name"]]
+
+        if "wife" in person and person["wife"] is not None:
+            current_person.wife = Person.people[person["wife"]]
+
+        if "husband" in person and person["husband"] is not None:
+            current_person.husband = Person.people[person["husband"]]
+
+    return person_list
